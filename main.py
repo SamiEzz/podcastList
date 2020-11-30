1cb068f288e1f02aaa7b836b35a77ea701f160f8
Sami E <sami.ezzerouali@amadeus.com>
2020-11-30 23:34:49 +0100

    readme

--- main.py
+++ main.py
@@ -14,9 +14,26 @@ def getLinks():
             print(lineStrip)
 
     return linksList
+def getReadme():
+    linksList=[]
+    # Using readlines() 
+    file1 = open('nplop', 'r') 
+    Lines = file1.readlines() 
+    print("# Podcasts Histoire :")
+    # Strips the newline character 
+    for line in Lines: 
+        lineStrip = line.strip()
+        title=lineStrip[lineStrip.find("histoire")+9:lineStrip.find(".mp3?")]
+        title=title.replace("-"," ")
+        link = lineStrip[lineStrip.find("http"):-1]
+        if(lineStrip.find("histoire")>0):
+            linksList.append(lineStrip)
+            #print("## "+title+" :")
+            print("## ["+title+"]"+"("+link+")")
 
 def main():
-    linksList = getLinks()
+    getReadme()
+    #linksList = getLinks()
     #system("wget "+str(linksList[0]))
 
 main()
